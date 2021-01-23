2b0cff55ce606016f04b32313534b65e2ccb1116
Maria Ortega <mariaortegapb@gmail.com>
2021-01-22 22:40:31 -0500

    created sql file

---
+++ employeeDB.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary VARCHAR(30) NULL,
+  department_id VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id VARCHAR(30) NULL,
+  manager_id VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Human", "Krewella", "Dance");
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("TRNDSTTR","Black Coast", "Dance");
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Who's Next", "The Who", "Classic Rock");
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Yellow Submarine", "The Beatles", "Classic Rock");
+
