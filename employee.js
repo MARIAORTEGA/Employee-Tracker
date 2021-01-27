9c3e99cb2505e6fb35b9514e08d786f0ac6386e2
Maria Ortega <mariaortegapb@gmail.com>
2021-01-26 21:44:51 -0500

    added changes

--- employee.js
+++ employee.js
@@ -15,5 +15,144 @@ connection.connect(function(err) {
   if (err) throw err;
     start();
 });
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// ask user if they would like to view, add, or update the departments, roles, employees
+function start() {
+  inquirer
+    .prompt({
+      name: "viewAdd",
+      type: "list",
+      message: "Would you like to [VIEW] [ADD] or [UPDATE] the  departments, roles, employees?",
+      choices: ["VIEW", "ADD", "UPDATE"]
+    })
+    .then(function(answer) {
+      // based on the answer, call view, add, or update function
+      if (answer.viewAdd === "VIEW") {
+        viewData();
+      }
+      else if(answer.viewAdd === "ADD") {
+        addEmployee();
+      } 
+      else if(answer.viewAdd === "UPDATE") {
+        updateEmployee();
+    }else{
+        connection.end();
+      }
+    });
+}
+
+
+///////////////////////////////////////////////////////////////////////to add employee
+function addEmployee() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "EmployeeFirst",
+        type: "input",
+        message: "Please add employee's first name:"
+      },
+      {
+        name: "EmployeeLast",
+        type: "input",
+        message: "Please add employee's last name:"
+      },
+      {
+        name: "EmployeeRole_id",
+        type: "input",
+        message: "Please add role id:"
+      },
+      {
+        name: "employeeManager_id",
+        type: "input",
+        message: "Please add manager id:",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      // after prompting, add new data into the db 
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answer.EmployeeFirst,
+          last_name: answer.EmployeeFirst,
+          role_id: answer.EmployeeRole_id,
+          manager_id: answer.EmployeeManager_id,
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Employee data created successfully");
+          // re-prompt the user for if they want to view, add or update
+          start();
+        }
+      );
+    });
+}
+//////////////////////////////////////////////////////to view departments, roles, employees
+function viewData() {
+  inquirer
+    .prompt({
+      name: "viewAdd",
+      type: "list",
+      message: "Would you like to view [departments], [roles], [employees]?",
+      choices: ["departments", "roles", "employee"]
+    }).then(function(answer) {
+      if (answer.viewAdd === "departments"){
+        connection.query("SELECT * FROM department", function(err, res) {
+           if (err) throw err;
+           console.table (res)
+          })
+        }
+  // connection.query("SELECT * FROM employee", function(err, res) {
+  //   if (err) throw err;
+  //   // Log all results of the SELECT statement
+  //   console.table(res);
+    connection.end(); 
+    })
+  }
+    
+
+//////////////////////////////////////////////////////to update departments, roles, employees
+
+function updateEmployee() {
+  console.log("Updating Employee\n");
+  var query = connection.query(
+    "UPDATE employee SET ? WHERE ?",
+    [
+      {
+        first_name:""
+      },
+      {
+        last_name: ""
+      },
+      {
+        role_id: ""
+      },
+      {
+        manager_id: ""
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee updated!\n");
+      // view data
+      viewData();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+
+
+
+
 
 
--- employeeDB.sql
+++ employeeDB.sql
@@ -1,3 +1,31 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO department (name)
+VALUES ("Legal"), ("Finance"), ("Sales");
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(15,4) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer",190000.00,11), ("Accountant", 125000.00, 22), ("Salesperson", 1000000.00,33);
+
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
@@ -5,28 +33,18 @@
   PRIMARY KEY (id)
 );
 
-INSERT INTO department (name)
-VALUES ("Legal"), ("Finance"), ("Sales");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 100, 10);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 200, 20);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 300, 30);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 400, 40);
 
-INSERT INTO roles (title)
-VALUES ("Lawyer"), ("Accountant"), ("Salesperson");
-INSERT INTO roles (salary)
-VALUES (190000), (125000), (100000);
-INSERT INTO roles (department_id )
-VALUES (001),(002),(003);
-
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Lennon", 100, 10);
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Paul", "McCartney", 200, 20);
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Ringo", "Starr", 300, 30);
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("George", "Harrison", 400, 40);
-
+USE employeeDB;
 
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
-
 
